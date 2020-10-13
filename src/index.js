0c1694ea19a1aea4962530157d43bbadb7d5e042
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-14 02:04:47 +0300

    feat: towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let evenBoolean = false
+  const result = []
+
+  if (matrix === undefined) return result
+
+  matrix.forEach(array => {
+    if (evenBoolean) {
+      array.reverse().forEach(n => result.push(n))
+      evenBoolean = false
+    } else {
+      array.forEach(n => result.push(n))
+      evenBoolean = true
+    }
+  })
+  return result
 }
